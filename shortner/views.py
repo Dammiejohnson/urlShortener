787b4d0041e95bc1792b229ffd5b1d08d8f8d5e6
Sanni Oluwadamilola <sannijohnson5@gmail.com>
2022-05-02 10:07:43 +0100

    my commit

--- shortner/views.py
+++ shortner/views.py
@@ -11,13 +11,15 @@ def index(request):
 
 def create(request):
     if request.method == 'POST':
-        link = request.POST['link']
+        url = request.POST['link']
+        if not url:
+            return
         uid = str(uuid.uuid4())[:5]
-        new_url = Url(link=link, uuid=uid)
+        new_url = Url(link=url, uuid=uid)
         new_url.save()
         return HttpResponse(uid)
 
 
 def go(request, pk):
     url_details = Url.objects.get(uuid=pk)
-    return redirect('https://' + url_details.link)
+    return redirect('' + url_details.link)
